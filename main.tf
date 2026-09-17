8c4ec0cf66c01b8fc7660e63dc906898c6fabc13
Himora <seun@HIMORA.localdomain>
2026-09-17 11:17:10 +0100

    Enable S3 bucket versioning

--- main.tf
+++ main.tf
@@ -30,6 +30,22 @@ resource "aws_s3_bucket" "demo" {
   }
 }
 
+# Enable versioning on the bucket
+resource "aws_s3_bucket_versioning" "demo" {
+  bucket = aws_s3_bucket.demo.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable versioning on the bucket
+resource "aws_s3_bucket_versioning" "demo" {
+  bucket = aws_s3_bucket.demo.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 # Output the bucket name
 output "bucket_name" {
   description = "Name of the created S3 bucket"
